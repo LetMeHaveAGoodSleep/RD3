02e4f24299d9164000319cd57ed72d313c2b2c37
LetMeHaveAGoodSleep <470890417@qq.com>
2024-07-07 22:00:09 +0800

    IndexView.xaml未完成 是使用Grid还是ScrollViewer还需确定

--- RD3.Shared/obj/Debug/net8.0/RD3.Shared.AssemblyInfo.cs
+++ RD3.Shared/obj/Debug/net8.0/RD3.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RD3.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+987450e3e16200471aa89a4fdc20f21c0e66357f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821d89eab4da3f7457b5cefbe2783948e7da4219")]
 [assembly: System.Reflection.AssemblyProductAttribute("RD3.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RD3.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RD3/obj/Debug/net8.0-windows7.0/RD3.AssemblyInfo.cs
+++ RD3/obj/Debug/net8.0-windows7.0/RD3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RD3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821d89eab4da3f7457b5cefbe2783948e7da4219")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300dd0561a1125e65c9a2d9f49fad43138ec7899")]
 [assembly: System.Reflection.AssemblyProductAttribute("RD3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RD3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
