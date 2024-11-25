101ed835a985ad81cbb1b81c0f53f21603d620ca
LetMeHaveAGoodSleep <470890417@qq.com>
2024-11-25 14:10:35 +0800

    增加控件旋转的附加属性

---
+++ RD3/Themes/Styles/Attach/RotationManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace RD3
+{
+    public class RotationManager
+    {
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.RegisterAttached("Angle", typeof(double), typeof(RotationManager), new PropertyMetadata(0.0, OnAngleChanged));
+
+        public static double GetAngle(DependencyObject obj)
+        {
+            return (double)obj.GetValue(AngleProperty);
+        }
+
+        public static void SetAngle(DependencyObject obj, double value)
+        {
+            obj.SetValue(AngleProperty, value);
+        }
+
+        private static void OnAngleChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            if(obj is UIElement element)
+            {
+                element.RenderTransformOrigin = new Point(0.5, 0.5);
+                element.RenderTransform = new RotateTransform((double)e.NewValue);
+            }
+        }
+    }
+}
--- RD3/obj/Debug/net8.0-windows7.0/RD3.AssemblyInfo.cs
+++ RD3/obj/Debug/net8.0-windows7.0/RD3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RD3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c36189c4bc111adf196553d0cf0b86606523281f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9e2ad77dee926c32a8c1ee283d06a6118bec10")]
 [assembly: System.Reflection.AssemblyProductAttribute("RD3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RD3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
